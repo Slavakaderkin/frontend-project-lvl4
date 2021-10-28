4f8637d8491f8d5cbe677e3fbb639cb2e76dc6fc
Slava Kaderkin <slavakaderkin@gmail.com>
2021-10-28 12:09:04 +0400

    add routing

---
+++ src/components/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
+
+import { Button, Navbar } from 'react-bootstrap';
+
+import LoginPage from './LoginPage.jsx';
+import MainPage from './MainPage.jsx';
+import NotFoundPage from './NotFoundPage.jsx';
+
+import useAuth from '../hooks/index.jsx';
+import authContext from '../contexts/index.jsx';
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  return (
+    <authContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </authContext.Provider>
+  );
+};
+
+const PrivateRoute = ({ children, path }) => {
+  const auth = useAuth();
+
+  return (
+    <Route path={path}>
+      {auth.loggedIn ? children : <Redirect to="/login" />}
+    </Route>
+  );
+};
+
+const LogOutButton = () => {
+  const auth = useAuth();
+
+  return (
+    auth.loggedIn
+      ? <Button onClick={auth.logOut}>Выйти</Button>
+      : null
+  );
+};
+
+const App = () => (
+  <AuthProvider>
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
+        <LogOutButton />
+      </Navbar>
+
+      <Switch>
+        <PrivateRoute exact path="/">
+          <MainPage />
+        </PrivateRoute>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="*">
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </Router>
+  </AuthProvider>
+);
+
+export default App;
