2c81f22c106cb5207f88847678c7ae21f08d0f1e
Slava Kaderkin <slavakaderkin@gmail.com>
2021-10-28 12:09:23 +0400

    mount app

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+// @ts-nocheck
+
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
+
+import '../assets/application.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App.jsx';
+
+if (process.env.NODE_ENV !== 'production') {
+  localStorage.debug = 'chat:*';
+}
+
+const container = document.querySelector('#chat');
+ReactDOM.render(<App />, container);
+
+console.log('it works!');
