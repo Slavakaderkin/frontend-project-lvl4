0e6deb665c41547637ba782494a41ddb6937ed28
Slava Kaderkin <slavakaderkin@gmail.com>
2021-11-28 14:54:46 +0400

    add getAuthHeader

---
+++ src/helpers/index.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/prefer-default-export
+export const getAuthHeader = () => {
+  const userId = JSON.parse(localStorage.getItem('userId'));
+  if (userId && userId.token) {
+    return { Authorization: `Bearer ${userId.token}` };
+  }
+
+  return {};
+};
