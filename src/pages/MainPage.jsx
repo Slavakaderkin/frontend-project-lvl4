a5a81a877c7a55bc360b5dc0c2fc16a048cb0f1a
Slava Kaderkin <slavakaderkin@gmail.com>
2021-11-29 12:02:26 +0400

    fix main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import Sidebar from '../components/Sidebar.jsx';
+import Chat from '../components/Chat.jsx';
 import { fetchData } from '../services/index.js';
 
 const MainPage = () => {
@@ -14,10 +15,7 @@ const MainPage = () => {
   return (
     <div className="row h-100">
       <Sidebar />
-      <main className="col-10 d-flex flex-column flex-fill justify-content-end">
-        <div>чат</div>
-        <div>инпут</div>
-      </main>
+      <Chat />
     </div>
   );
 };
