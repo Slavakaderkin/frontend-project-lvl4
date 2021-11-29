c33a1a75a108b45c066af7defc48cbbf73f8f35c
Slava Kaderkin <slavakaderkin@gmail.com>
2021-11-29 12:01:52 +0400

    add sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import cn from 'classnames';
+
+import { setCurrentChannelId } from '../store/slices/channelsSlice.js';
 
 const Sidebar = () => {
-  const channels = useSelector(({ channels }) => {
-    const { allIds, byId } = channels;
+  const dispatch = useDispatch();
+
+  const channels = useSelector((state) => {
+    const { allIds, byId } = state.channels;
     return allIds.map((id) => byId[id]);
   });
 
-  const currentChannelId = useSelector(({ channels }) => channels.currentChannelId);
+  const currentChannelId = useSelector((state) => state.channels.currentChannelId);
+
+  const handleClick = (id) => (e) => {
+    e.preventDefault();
+    dispatch(setCurrentChannelId(id));
+  };
 
   return (
     <section className="p-4 col-2 overflow-auto border-right bg-white">
       <ul className="list-group list-group-flush">
-        {channels.map(({ id, name }) => <li className="list-group-item" key={id}>{`# ${name}`}</li>)}
+        {channels.map(({ id, name }) => {
+          const isActive = id === currentChannelId;
+          const classes = cn({
+            'list-group-item': true,
+            border: isActive,
+            'border-primary': isActive,
+            rounded: true,
+          });
+          return (
+            <li className={classes} key={id} onClick={handleClick(id)} aria-hidden="true">
+              {`# ${name}`}
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
