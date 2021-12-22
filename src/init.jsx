aa9b65e3d816f2761caa27ef20b5c5307e3430d7
Slava Kaderkin <slavakaderkin@gmail.com>
2021-12-22 14:38:06 +0400

    configure rollbar

--- src/init.jsx
+++ src/init.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import i18n from 'i18next';
 import { initReactI18next, I18nextProvider } from 'react-i18next';
 import { injectStyle } from 'react-toastify/dist/inject-style';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 import { Provider } from 'react-redux';
 import AuthProvider from './components/AuthProvider.jsx';
@@ -13,6 +14,15 @@ import store from './store';
 import resources from './locales';
 
 const init = () => {
+  const rollbarConfig = {
+    accessToken: '0b4d2701c2034940ab166c97705afa18',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      environment: 'production',
+    },
+  };
+
   const i18nInstance = i18n.createInstance();
 
   i18nInstance
@@ -29,15 +39,19 @@ const init = () => {
   injectStyle();
 
   return (
-    <Provider store={store}>
-      <I18nextProvider i18n={i18nInstance}>
-        <AuthProvider>
-          <SocketProvider>
-            <App />
-          </SocketProvider>
-        </AuthProvider>
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18nInstance}>
+            <AuthProvider>
+              <SocketProvider>
+                <App />
+              </SocketProvider>
+            </AuthProvider>
+          </I18nextProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
